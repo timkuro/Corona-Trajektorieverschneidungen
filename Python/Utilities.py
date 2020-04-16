54f05851c05b9db3fe75d881a16ce40ccedadd94
Tim Kurowski <tim.kurowski@hs-bochum.de>
2020-04-16 16:02:10 +0200

    implement read_kml_line

--- Python/Utilities.py
+++ Python/Utilities.py
@@ -1,5 +1,13 @@
+import xml.etree.ElementTree as ET
+
+# reads the kml file and prepares data for further operations
 def read_kml_line(path):
-    pass
+    tree = ET.parse(path)
+    root = tree.getroot()
+    if "Track" in root[0][0][1].tag:
+        return root[0][0][1]
+    else:
+        raise Exception('no track-tag found in KML')
 
 def split_line(input_line):
     pass
@@ -12,3 +20,5 @@ def intersect_geom(linestrings):
 
 def intersect_time(crosspoints):
     pass
+
+print(read_kml_line(r"D:\Uni-Lokal\GI-Projekt Corona\hs-bochum.de\Christian Koert - GI_Projekt_Wytzisk\Standortverlauf_Tim_Juli2019.kml"))
