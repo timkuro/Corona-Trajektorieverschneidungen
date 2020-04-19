554df978bec55ac02f6759cd7db5f00a81f98d8d
ThomasB <thomas.blindert@hs-bochum.de>
2020-04-19 12:28:17 +0200

    implement Linestring.intersect() using arcpy Geometry.intersect()

--- Python/Geometries.py
+++ Python/Geometries.py
@@ -1,3 +1,5 @@
+import arcpy
+
 class Point:
     def __init__(self, x, y, timestamp):
         self.x = x
@@ -13,7 +15,12 @@ class Linestring:
         self.endpoint = endpoint
 
     def intersect(self, other_line):
-        pass
+        myline = arcpy.Polyline(arcpy.Array([arcpy.Point(self.startpoint.x, self.startpoint.y), arcpy.Point(self.endpoint.x, self.endpoint.y)]))
+        otherline = arcpy.Polyline(arcpy.Array([arcpy.Point(other_line.startpoint.x, other_line.startpoint.y), arcpy.Point(other_line.endpoint.x, other_line.endpoint.y)]))
+        if myline.intersect(otherline, 1):
+            return True
+        else:
+            return False
 
     def __str__(self):
         return str(self.startpoint) + " " + str(self.endpoint)
@@ -25,3 +32,10 @@ class Crosspoint:
         self.lambda2 = lambda2
         self.line1 = line1
         self.line2 = line2
+
+
+'''test = (Linestring(Point(1,1, None), Point(1,2, None)).intersect(Linestring(Point(2,1, None), Point(2, 2, None))))
+print(type(test))
+if not test:
+    print('keine geometrie')
+#print(str(test.firstPoint.X) + " " + str(test.firstPoint.Y))'''
