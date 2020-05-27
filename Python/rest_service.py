393dc74ad0f7b3d003086424c40f0ac7f5d4e93c
Tim Kurowski <tim.kurowski@hs-bochum.de>
2020-05-27 19:29:31 +0200

    post raw

--- Python/rest_service.py
+++ Python/rest_service.py
@@ -4,47 +4,41 @@ import cherrypy
 import os
 from Utilities import read_kml_line
 
-if __name__ == "__main__":
-    path_timmy=r"C:\Users\Tim\hs-bochum.de\Christian Koert - GI_Projekt_Wytzisk\Standortverlauf_Juli_2019"
-    path_tommy=r"D:\hs-bochum.de\Christian Koert - GI_Projekt_Wytzisk\Standortverlauf_Juli_2019"
-    path_kort=r"C:\Users\chris\OneDrive - hs-bochum.de\GI_Projekt_Wytzisk\Standortverlauf_Juli_2019"
-
-    if(os.environ['USERNAME'] == "Thomas"):
-        path = path_tommy
-    elif(os.environ['USERNAME'] == "Tim"):
-        path = path_timmy
-    elif(os.environ['USERNAME'] == "chris"):
-        path = path_kort
-
-    export_path = path.split("\\")
-    trajectories = []
-
-    datei1 = read_kml_line(path[:-len(export_path[-1])] + "Standortverlauf_Juli_2019\Standortverlauf_Christian_Juli2019.kml")
-    datei2 = read_kml_line(path[:-len(export_path[-1])] + "Standortverlauf_Juli_2019\Standortverlauf_Thomas_Juli2019.kml")
-    datei3 = read_kml_line(path[:-len(export_path[-1])] + "Standortverlauf_Juli_2019\Standortverlauf_Tim_Juli2019.kml")
+lengths = []
+trajectories = []
+infected_persons = []
+test_persons = []
 
 '''https://docs.cherrypy.org/en/3.3.0/tutorial/REST.html#multiple-resources'''
 
-class Trajectories:
+class Infected:
 
     exposed = True
-    trajectories.append(datei1)
-    trajectories.append(datei2)
 
     def GET(self):
-         return(f'Here are the lengths of all trajectories: {str(len(trajectories[0]) / 2 - 1)}, {str(len(trajectories[1]) / 2 - 1)}')
+        return infected_persons
 
+    def POST(self):
+        rawData = cherrypy.request.body.read(int(cherrypy.request.headers['Content-Length']))
+        infected_persons.append(rawData)
+        return 'added infected person: \n' + rawData
 
-    def POST(self, path):
-        new_trajectory = read_kml_line(path[:-len(export_path[-1])] + path)
-        trajectories.append(new_trajectory)
+class Test_person:
 
-        return (f'Created a new trajectory with the length: {str(len(trajectories[len(trajectories)-1]) / 2 - 1)}')
+    exposed = True
+
+    def GET(self):
+         return test_persons
+
+    def POST(self):
+        rawData = cherrypy.request.body.read(int(cherrypy.request.headers['Content-Length']))
+        test_persons.append(rawData)
+        return 'added test person'
 
 if __name__ == '__main__':
 
     cherrypy.tree.mount(
-        Trajectories(), '/api/trajectories',
+        Test_person(), '/api/test_person',
         {'/':
             {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
         }
