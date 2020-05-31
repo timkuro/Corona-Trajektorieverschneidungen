dda406019cde821e4f6d50583453593beb588d50
Tim Kurowski <tim.kurowski@hs-bochum.de>
2020-05-31 12:46:27 +0200

    integrate procession into cherrypy

--- Python/rest_service.py
+++ Python/rest_service.py
@@ -1,48 +1,68 @@
-import random
-import string
 import cherrypy
+from Utilities import *
 import os
-from Utilities import read_kml_line
 
-lengths = []
-trajectories = []
 infected_persons = []
 test_persons = []
 
-'''https://docs.cherrypy.org/en/3.3.0/tutorial/REST.html#multiple-resources'''
-
-class Infected:
-
+class Infected_person:
     exposed = True
 
     def GET(self):
-        return infected_persons
+        return str(len(infected_persons))
 
     def POST(self):
         rawData = cherrypy.request.body.read(int(cherrypy.request.headers['Content-Length']))
-        infected_persons.append(rawData)
-        return 'added infected person: \n' + rawData
+        rawData_xml = read_kml_line(rawData)
+
+        i = (len)(infected_persons) + 1
+        splitted_lines = split_line(rawData_xml, '2019-07-01T00:00:00Z', '2019-08-01T00:00:00Z', (str(i)))
+        infected_persons.append(splitted_lines)
+        return splitted_lines.__str__()
+
 
 class Test_person:
-
     exposed = True
 
     def GET(self):
-         return test_persons
+        return str(len(test_persons))
 
     def POST(self):
         rawData = cherrypy.request.body.read(int(cherrypy.request.headers['Content-Length']))
-        test_persons.append(rawData)
-        return 'added test person'
+        rawData_xml = read_kml_line(rawData)
+
+        i = (len)(infected_persons) + 1
+        splitted_lines = split_line(rawData_xml, '2019-07-01T00:00:00Z', '2019-08-01T00:00:00Z', (str(i)))
+        test_persons.append(splitted_lines)
+
+        infectedLines = []
+        for element in infected_persons:
+            infectedLines += element
+        healthyLines = splitted_lines
+
+        infectedLines, healthyLines = boundingBox_intersection(infectedLines, healthyLines)
+
+        result_geom = intersect_geom(infectedLines, healthyLines, distance=10)
+        result_time = intersect_time(result_geom, delta=datetime.timedelta(minutes=15))
+
+        convert_crossline_to_shapefile(result_time, "C:\\Users\\" + os.environ['USERNAME'] + "\\", "corona_contacts")
+
+        return f"You may mave had {len(result_time)} contacts with infected persons. To see the positions, open " \
+               f"C:\\Users\\" + os.environ['USERNAME'] + "\\", "corona_contacts.shp"
+
 
 if __name__ == '__main__':
-
     cherrypy.tree.mount(
         Test_person(), '/api/test_person',
         {'/':
-            {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
-        }
+             {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
+         }
     )
+    cherrypy.tree.mount(
+        Infected_person(), '/api/infected_person',
+        {'/':
+             {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
+         })
 
     cherrypy.engine.start()
     cherrypy.engine.block()
